8f5b19e802f7ea633db725598ca7b8c39fbf0dff
SergeyZakurakin <pricezak@gmail.com>
2025-02-07 15:35:48 -0500

    refactor

--- PetTranslator/Model/AudioRecorder.swift
+++ PetTranslator/Model/AudioRecorder.swift
@@ -5,4 +5,37 @@
 //  Created by Sergey Zakurakin on 2/7/25.
 //
 
-import Foundation
+import AVFoundation
+
+class AudioRecorder: NSObject, ObservableObject {
+    var audioRecorder: AVAudioRecorder?
+    @Published var isRecording = false
+    
+    func startRecording() {
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(.playAndRecord, mode: .default, options: .defaultToSpeaker)
+            try session.setActive(true, options: .notifyOthersOnDeactivation)
+            
+            let settings: [String: Any] = [
+                AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                AVSampleRateKey: 12000,
+                AVNumberOfChannelsKey: 1,
+                AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+            ]
+            
+            let url = FileManager.default.temporaryDirectory.appendingPathComponent("recording.m4a")
+            audioRecorder = try AVAudioRecorder(url: url, settings: settings)
+            audioRecorder?.record()
+            isRecording = true
+        } catch {
+            print("Failed to start recording: \(error.localizedDescription)")
+        }
+    }
+    
+    func stopRecording() {
+        audioRecorder?.stop()
+        isRecording = false
+    }
+}
+
