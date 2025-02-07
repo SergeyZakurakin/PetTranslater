3917499071ea2968b2e33589540e93794d8808c7
SergeyZakurakin <pricezak@gmail.com>
2025-02-07 15:31:00 -0500

    fix bugs

--- PetTranslator/View/ProcessingView.swift
+++ PetTranslator/View/ProcessingView.swift
@@ -5,4 +5,38 @@
 //  Created by Sergey Zakurakin on 2/7/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct ProcessingView: View {
+    @ObservedObject var viewModel: TranslatorViewModel
+    @State private var isProcessing = true
+
+    var body: some View {
+        VStack {
+            Text("Processing...")
+                .font(.title)
+                .padding()
+            
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle())
+                .scaleEffect(2)
+
+            if !isProcessing {
+                NavigationLink(destination: ResultView(viewModel: viewModel)) {
+                    Text("View Result")
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+        }
+        .onAppear {
+            viewModel.processTranslation()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                isProcessing = false
+            }
+        }
+    }
+}
+
