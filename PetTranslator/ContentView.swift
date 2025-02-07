d0c8e91ae8e4184b3c4a010cc90a1cd30001512a
SergeyZakurakin <pricezak@gmail.com>
2025-02-07 15:05:05 -0500

    Initial Commit

---
+++ PetTranslator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetTranslator
+//
+//  Created by Sergey Zakurakin on 2/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PetTranslator/PetTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetTranslatorApp.swift
+//  PetTranslator
+//
+//  Created by Sergey Zakurakin on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PetTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
