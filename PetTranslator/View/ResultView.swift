d932ee8e79c288bc6e8918f803e652e16d9b789c
SergeyZakurakin <pricezak@gmail.com>
2025-02-07 15:33:36 -0500

    fix

--- PetTranslator/View/ResultView.swift
+++ PetTranslator/View/ResultView.swift
@@ -5,4 +5,28 @@
 //  Created by Sergey Zakurakin on 2/7/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct ResultView: View {
+    @ObservedObject var viewModel: TranslatorViewModel
+
+    var body: some View {
+        VStack {
+            Text("Translation Result")
+                .font(.title)
+                .padding()
+            
+            Text(viewModel.translatedText)
+                .font(.headline)
+                .padding()
+            
+            NavigationLink(destination: TranslatorView()) {
+                Text("Back to Home")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+    }
+}
