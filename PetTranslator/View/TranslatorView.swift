a5bc8e6e947dba1314408588a3a5cf838b287807
SergeyZakurakin <pricezak@gmail.com>
2025-02-07 19:48:58 -0500

    change icons

--- PetTranslator/View/TranslatorView.swift
+++ PetTranslator/View/TranslatorView.swift
@@ -14,7 +14,7 @@ struct TranslatorView: View {
         
         
         ZStack {
-          
+            
             NavigationView {
                 VStack {
                     Text("Translator")
@@ -32,45 +32,62 @@ struct TranslatorView: View {
                     .font(.system(size: 16))
                     .frame(maxWidth: .infinity)
                     
-                  
+                    
                     HStack {
                         NavigationLink(destination: RecordingView(viewModel: viewModel)) {
-                            Image(systemName: "mic.fill")
-                                .resizable()
-                                .frame(width: 80, height: 80)
-                                .padding()
+                            
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(Color.white)
+                                    .frame(width: 178, height: 176)
+                                    .shadow(color: .gray.opacity(0.8), radius: 5, x: 0, y: 5)
+                                VStack {
+                                    Image("microphone-2")
+                                        .resizable()
+                                        .frame(width: 70, height: 70)
+                                        .padding()
+                                    Text("Start to speak")
+                                        .foregroundStyle(.black)
+                                        .fontWeight(.bold)
+                                }
+                            }
                         }
                         
                         
-                        VStack {
-                            Button(action: { viewModel.selectAnimal("dog") }) {
-                                Image(systemName: "dog.fill")
-                                    .resizable()
-                                    .frame(width: 50, height: 50)
-                                    .opacity(viewModel.selectedAnimal == "dog" ? 1.0 : 0.5)
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color.white)
+                                .frame(width: 107, height: 176)
+                                .shadow(color: .gray.opacity(0.8), radius: 5, x: 0, y: 5)
+                            VStack {
+                                Button(action: { viewModel.selectAnimal("dog") }) {
+                                    Image("Pet1")
+                                        .resizable()
+                                        .frame(width: 83, height: 70)
+                                        .opacity(viewModel.selectedAnimal == "dog" ? 1.0 : 0.5)
+                                }
+                                
+                                Button(action: { viewModel.selectAnimal("cat") }) {
+                                    Image("Pet2")
+                                        .resizable()
+                                        .frame(width: 83, height: 70)
+                                        .opacity(viewModel.selectedAnimal == "cat" ? 1.0 : 0.5)
+                                }
+
                             }
-                            .padding()
-                            
-                            Button(action: { viewModel.selectAnimal("cat") }) {
-                                Image(systemName: "cat.fill")
-                                    .resizable()
-                                    .frame(width: 50, height: 50)
-                                    .opacity(viewModel.selectedAnimal == "cat" ? 1.0 : 0.5)
-                            }
-                            .padding()
                         }
                     }
                     .padding(.top, 58)
                     
                     // Большая иконка выбранного животного
-                    Image(systemName: viewModel.selectedAnimal == "dog" ? "dog.fill" : "cat.fill")
+                    Image(viewModel.selectedAnimal == "dog" ? "dog" : "cat")
                         .resizable()
-                        .frame(width: 150, height: 150)
+                        .frame(width: 200, height: 200)
                         .padding()
                         .padding(.top, 51)
                     
                     // Кнопка "Микрофон"
-                   
+                    
                     
                     Spacer()
                 }
@@ -83,7 +100,7 @@ struct TranslatorView: View {
                         startPoint: .bottom,
                         endPoint: .top
                     )
-                  
+                    
                     .ignoresSafeArea()
                 )
             }
